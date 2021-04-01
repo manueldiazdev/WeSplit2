90cd3cf416de9ee0c81b9de05f4e123e5dae47a5
Manny <mhdiaz20@gmail.com>
2021-04-01 14:27:50 -0700

    Initial Commit

---
+++ WeSplit2/WeSplit2App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit2App.swift
+//  WeSplit2
+//
+//  Created by Manuel Diaz on 4/1/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
